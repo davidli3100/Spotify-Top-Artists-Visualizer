3057a6b0df1e8c21cf1d68a86b46fced3c8983f6
davidli3100 <david.li3100@gmail.com>
2018-11-27 12:14:23 -0500

    push mostof the stuff

---
+++ src/Viewer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+/**
+ * default class, written completely in P5.js combined with a Node.JS auth server
+ * gets the auth token and bearer for the user, and then uses it to make asynchronous calls to display p5 data
+ */
+export default class Viewer extends Component {
+    render() {
+        return (
+            <div>test</div>
+        );
+    }
+}
---
+++ views/P5Wrapper.js
@@ -0,0 +1,2 @@
+//basic P5 wrapper for React.JS in a Node.JS runtime, reused with a few modifications from my first assignment
+import React, { Component } from 'react';
