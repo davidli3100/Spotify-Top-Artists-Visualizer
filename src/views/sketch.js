56dda0f663a350a22b60b1505cb8acf8c785174c
davidli3100 <davidli3100@gmail.com>
2018-12-10 14:26:59 -0500

    this better work

--- src/views/sketch.js
+++ src/views/sketch.js
@@ -78,25 +78,29 @@ export default function sketch(p) {
         //maps one range to another
         const mapToColour = (num, in_min, in_max, out_min, out_max) => {
             return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-          }
+        }
 
         /**
          * maps all the genres to return a value based on popularity, or randint if it's not a "main stream genre" 
          */
         const genreMap = (genres) => {
-            for(var j = 0; j < genres.length; j++) {
-                if(genres[j].includes('pop')) {
-                    //just returning values based on the popularity of said genre
-                    return 100
-                } else if (genres[j].includes('rap') || genres[j].includes('hip')) {
-                    return 95
-                } else if (genres[j].includes('rock')) {
-                    return 90
-                } else {
-                    //returns randint if it's not a popular genre
-                    return Math.floor(Math.random() * (100 - 0 + 1)) + 0;
+            try {
+                for (var j = 0; j < genres.length; j++) {
+                    if (genres[j].includes('pop')) {
+                        //just returning values based on the popularity of said genre
+                        return 100
+                    } else if (genres[j].includes('rap') || genres[j].includes('hip')) {
+                        return 95
+                    } else if (genres[j].includes('rock')) {
+                        return 90
+                    } else {
+                        //returns randint if it's not a popular genre
+                        return Math.floor(Math.random() * (100 - 0 + 1)) + 0;
 
+                    }
                 }
+            } catch (err) {
+                return Math.floor(Math.random() * (100 - 0 + 1)) + 0;
             }
         }
         var xOffset = 125;
@@ -109,56 +113,74 @@ export default function sketch(p) {
             //render rect for text
             //fills the shape based on the mapToColour function that maps followers/popularity/genres to color
             p.fill(mapToColour(data[i].popularity, 0, 100, 0, 255), mapToColour(data[i].followers, 0, 35000000, 0, 255), genreMap(data[i].genres))
-            p.rect(xOffset*i+25, yOffset-50, 100, 100)
+            p.rect(xOffset * i + 25, yOffset - 50, 100, 100)
             //render text
             p.textSize(20);
             p.noStroke()
             p.fill(255)
-            p.text(data[i].name.fName, xOffset*i+40, yOffset)
-            //truncate text just in case
-            // uses ALOT of try/catch statements since P5.js has many many issues
-            try{
+            try {
                 //find length of the second portion of the artist's name (if available)
-            var lNameLength = data[i].name.lName.length
-            } catch(err) {
+                var fNameLength = data[i].name.fName.length
+            } catch (err) {
                 console.log(err)
             }
-            //if last name length is greater than 7, we have to truncate
-            if(lNameLength > 7) {
+            if (fNameLength > 7) {
                 //try catch again because p5, that's why
                 try {
                     //truncating lastname using substrings, then appending ellipses to it
-                var truncatedLastName = data[i].name.lName.substring(0, truncateMaxLength-2)
-                p.text(truncatedLastName + '...', xOffset*i+40, yOffset+20)
-                } catch(err) {
+                    var truncatedFirstName = data[i].name.fName.substring(0, truncateMaxLength - 2)
+                    p.text(truncatedFirstName + '...', xOffset * i + 40, yOffset)
+                } catch (err) {
+                    console.log("error: " + data[i].name.lName)
+                }
+            } else {
+                p.text(data[i].name.fName, xOffset * i + 40, yOffset)
+            }
+            //truncate text just in case
+            // uses ALOT of try/catch statements since P5.js has many many issues
+            try {
+                //find length of the second portion of the artist's name (if available)
+                var lNameLength = data[i].name.lName.length
+            } catch (err) {
+                console.log(err)
+            }
+
+            //if last name length is greater than 7, we have to truncate
+            if (lNameLength > 7) {
+                //try catch again because p5, that's why
+                try {
+                    //truncating lastname using substrings, then appending ellipses to it
+                    var truncatedLastName = data[i].name.lName.substring(0, truncateMaxLength - 2)
+                    p.text(truncatedLastName + '...', xOffset * i + 40, yOffset + 20)
+                } catch (err) {
                     console.log("error: " + data[i].name.lName)
                 }
             } else {
                 //if their last name is shorter, just draw it in the correct position
-                p.text(data[i].name.lName, xOffset*i+40, yOffset+20)
+                p.text(data[i].name.lName, xOffset * i + 40, yOffset + 20)
             }
             //check if they click on the artist node, then load the artists data
-            if(customMouseX > xOffset*i+25  && customMouseX < xOffset*i+125 && customMouseY > yOffset-50 && customMouseY < yOffset+50) {
+            if (customMouseX > xOffset * i + 25 && customMouseX < xOffset * i + 125 && customMouseY > yOffset - 50 && customMouseY < yOffset + 50) {
                 //custom var to store genres for each artist so we can display genres in a nice format
                 var genres = []
-                for(var genre in data[i].genres) {
+                for (var genre in data[i].genres) {
                     genres.push(data[i].genres[genre])
                 }
 
-                p.text("Followers:\n" + data[i].followers + "\nPopularity:\n" + data[i].popularity + "\nGenres:\n" + genres.join("\n"), xOffset*i+30, yOffset+90)
-                
+                p.text("Followers:\n" + data[i].followers + "\nPopularity:\n" + data[i].popularity + "\nGenres:\n" + genres.join("\n"), xOffset * i + 30, yOffset + 90)
+
             }
         }
-        
+
 
     }
 
     //custom function to track mouseX and mouseY position when clicked
-    p.mouseClicked = function() {
+    p.mouseClicked = function () {
         customMouseX = p.mouseX;
         customMouseY = p.mouseY;
-        
-      }
+
+    }
 
     p.getUser = function (id) {
         p.loadJSON("https://spotifytrackdb.firebaseio.com/users/" + id + ".json?print=pretty", p.resolveUser);
@@ -186,10 +208,10 @@ export default function sketch(p) {
             console.log(
                 "name: " + name + "\n" +
                 "followers: " + followers + "\n" +
-                "popularity: " + popularity + "\n" + 
+                "popularity: " + popularity + "\n" +
                 "genres: " + genres
             )
-            
+
             rects.push({
                 name: {
                     fName: name[0],
